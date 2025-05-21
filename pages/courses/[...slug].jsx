f7501555789fc9df49a4559cc34ae44b57511708
roovin <roovinbansal@gmail.com>
2025-05-21 12:01:36 +0530

    Update slug

--- pages/courses/[...slug].jsx
+++ pages/courses/[...slug].jsx
@@ -20,22 +20,22 @@ export default function Course() {
                         courseData?.map((item, i) => {
                             return (
                                 <>
-                                {item?.title === isValue ? 
-                                    <div className="textWithImage flex w-[calc(100%+40px)] ml-[-20px]" key={i}>
-                                        <div className="content w-[calc(50%-20px)] mx-[20px]">
-                                            <h1>{item?.title}</h1>
-                                            <p>{item?.description}</p>
-                                            <div className="btnWrap">
-                                                <button>{item?.btn}</button>
+                                    {item?.title === isValue ?
+                                        <div className="textWithImage flex w-[calc(100%+40px)] ml-[-20px]" key={i}>
+                                            <div className="content w-[calc(50%-20px)] mx-[20px]">
+                                                <h1>{item?.title}</h1>
+                                                <p>{item?.description}</p>
+                                                <div className="btnWrap mt-[20px]">
+                                                    <button className='bg-[#001f3d] text-white px-6 py-3 rounded-md hover:bg-blue-800 transition duration-300'>{item?.btn}</button>
+                                                </div>
+                                            </div>
+                                            <div className="imageWrap  w-[calc(50%-20px)] mx-[20px]">
+                                                <div className="image">
+                                                    <Image src={item?.image} alt={item?.alt} className='rounded-[5px]' width={450} height={320} />
+                                                </div>
                                             </div>
                                         </div>
-                                        <div className="imageWrap  w-[calc(50%-20px)] mx-[20px]">
-                                            <div className="image">
-                                                <Image src={item?.image} alt={item?.alt} className='rounded-[5px]' width={450} height={320} />
-                                            </div>
-                                        </div>
-                                    </div>
-                                : '' }
+                                        : ''}
                                 </>
                             )
                         })
@@ -45,7 +45,22 @@ export default function Course() {
             </section>
             <section className="certificate">
                 <div className="container">
-                    
+                    {
+                        courseData?.map((item, i) => {
+                            return (
+                                <>
+                                    {item?.title === isValue ?
+                                        <div className="certificate bg-gray-100 text-center py-[30px]">
+                                            <h2>{item?.certifiedTitle}</h2>
+                                            <p>{item?.para}</p>
+                                            <Image src={item?.certiImage} alt={item?.certiAlt} className='mx-auto my-[20px]' width={300} height={300} />
+                                            <p>{item?.para1}</p>
+                                        </div>
+                                    : ''}
+                                </>
+                            )
+                        })
+                    }
                 </div>
             </section>
         </>
