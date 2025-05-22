62b643a126cbfbf7c8ef813d62e2ec969b7f9fdc
roovin <roovinbansal@gmail.com>
2025-05-22 13:22:22 +0530

    Update

--- pages/courses/[...slug].jsx
+++ pages/courses/[...slug].jsx
@@ -21,15 +21,15 @@ export default function Course() {
                             return (
                                 <div key={i}>
                                     {item?.title === isValue ?
-                                        <div className="textWithImage flex w-[calc(100%+40px)] ml-[-20px]" >
-                                            <div className="content w-[calc(50%-20px)] mx-[20px]">
+                                        <div className="textWithImage flex max-sm:flex-wrap w-[calc(100%+40px)] ml-[-20px] max-md:w-[calc(100%+20px)] max-md:ml-[-10px] max-sm:w-full max-sm:ml-0" >
+                                            <div className="content w-[calc(50%-20px)] mx-[20px] max-sm:w-full max-sm:mx-0">
                                                 <h1>{item?.title}</h1>
                                                 <p>{item?.description}</p>
                                                 <div className="btnWrap mt-[20px]">
                                                     <button className='bg-[#001f3d] text-white px-6 py-3 rounded-md hover:bg-blue-800 transition duration-300'>{item?.btn}</button>
                                                 </div>
                                             </div>
-                                            <div className="imageWrap  w-[calc(50%-20px)] mx-[20px]">
+                                            <div className="imageWrap  w-[calc(50%-20px)] mx-[20px] max-sm:mt-[20px] max-sm:w-full max-sm:mx-0">
                                                 <div className="image">
                                                     <Image src={item?.image} alt={item?.alt} className='rounded-[5px]' width={450} height={320} />
                                                 </div>
@@ -50,7 +50,7 @@ export default function Course() {
                             return (
                                 <div key={i}>
                                     {item?.title === isValue ?
-                                        <div className="certificate bg-gray-100 text-center py-[30px]">
+                                        <div className="certificate bg-gray-100 text-center py-[30px] max-md:p-[20px]">
                                             <h2>{item?.certifiedTitle}</h2>
                                             <p>{item?.para}</p>
                                             <Image src={item?.certiImage} alt={item?.certiAlt} className='mx-auto my-[20px]' width={300} height={300} />
