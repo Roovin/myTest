729037579e4dc3b8e90f5b26be77e56ed2bb963e
Roovin <roovinbansal@gmail.com>
2025-06-14 15:00:22 +0530

    Update

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,11 +13,32 @@ import { counterData } from '@/public/data/counterData'
 import { courseData } from '@/public/data/courseData'
 import { quoteData } from '@/public/data/quoteData'
 import { useEffect } from 'react'
+import { NextSeo } from 'next-seo'
 
 
 export default function index() {
   return (
     <>
+      <NextSeo 
+        title={'Think academy'}
+				description={'Think Academy'}
+				canonical='https://my-test-gilt.vercel.app/'
+				openGraph={{
+					url: 'https://my-test-gilt.vercel.app/',
+					title: 'Think Academy',
+					description: 'Think Academy',
+					images: [
+						{
+							url: 'https://my-test-gilt.vercel.app/',
+							width: 800,
+							height: 600,
+							alt: 'ThinkAcademy',
+							type: 'image/jpeg',
+						},
+					],
+					siteName: 'ThinkAcademy',
+				}}
+      />
       <HeroBanner data={heroBannerData}/>
       <TabWithImage data={tabWithImageData}/>
       <Counter data={counterData} />
