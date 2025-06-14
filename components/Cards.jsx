0071aa091995f5870c9e82ce0efaf4f6f1496a78
Roovin <roovinbansal@gmail.com>
2025-06-14 09:01:33 +0530

    Update cards

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -25,16 +25,16 @@ export default function cards() {
             </div>
             <div className="cardsWrap flex flex-wrap w-[calc(100%+20px)] ml-[-10px]" data-aos="fade-up">
                 {
-                    courses?.course?.map((item) => {
+                    courses?.course?.map((item, index) => {
                         return (
-                              <div className="card w-[calc(33.33%-20px)] mx-[10px] rounded-[5px] border-black border-[2px] border-solid max-md:w-[calc(50%-20px)] max-md:mx-[10px] max-md:last:mt-[20px] max-sm:w-full max-sm:mx-0 max-sm:mb-[20px] max-sm:last:mt-0">
+                              <div key={index} className="card w-[calc(33.33%-20px)] mx-[10px] rounded-[5px] border-black border-[2px] border-solid max-md:w-[calc(50%-20px)] max-md:mx-[10px] max-md:last:mt-[20px] max-sm:w-full max-sm:mx-0 max-sm:mb-[20px] max-sm:last:mt-0">
                                 <div className="imgWrap w-full h-[240px]">
                                     <Image src={item?.image_name} alt='' className='w-full h-full object-cover rounded-t-[3px]' width={300} height={300} />
                                 </div>
                                 <div className="content p-[20px]">
                                 <h3 >{item?.course_name}</h3>
                                 <p className='my-[10px] !font-[500]'><b>Price:- </b> {item?.price}</p>
-                                    <div className="btnWrap">
+                                    <div className="btnWrap mt-[30px]">
                                         <Link href={'/'} className='text-black bg-yellow-500 font-[600] rounded-full inline-block px-6 py-3 transition-all duration-300 hover:bg-yellow-600 '>{item?.button}</Link>
                                     </div>
                                 </div>
